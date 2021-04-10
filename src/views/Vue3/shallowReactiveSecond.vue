2fd2f74a0abe7edead05172fd7ee86de56ef63c8
苏震 <suzhen@viewhigh.com>
2021-04-10 09:29:48 +0800

    shallowreactive api

---
+++ src/views/Vue3/shallowReactiveSecond.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>Shallow reactive</p>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  setup() {
+    const obj = {
+      a: 1,
+      first: {
+        b: 2,
+        second: {
+          c: 3
+        }
+      }
+    }
+    // 将 obj 对象中所有的属性值都转换为响应式数据
+    const state = reactive(obj)
+    console.info('state', state)
+    console.info('state', state.first)
+    console.info('state', state.first.second)
+    /**
+     * 打印结果:
+     * Proxy {a: 1, first: {…}}
+     * Proxy {b: 2, second: {…}}
+     * Proxy {c: 3}
+     */
+  }
+}
+</script>
