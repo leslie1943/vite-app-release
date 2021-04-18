abff91d43fa423bcc599e9f00913694240befd60
苏震 <suzhen@viewhigh.com>
2021-04-18 19:59:54 +0800

    env files

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,5 @@ import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 
 createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
+
+console.info(import.meta.env)
